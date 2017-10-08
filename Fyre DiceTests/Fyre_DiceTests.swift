477ce6d88fcff3872b31ecb0d3159a97f30795d4
Wayne Ohmer <wayneohmer@gmail.com>
2017-10-07 20:24:55 -0700

    First commit

---
+++ Fyre DiceTests/Fyre_DiceTests.swift
@@ -0,0 +1,36 @@
+//
+//  Fyre_DiceTests.swift
+//  Fyre DiceTests
+//
+//  Created by Wayne Ohmer on 10/7/17.
+//  Copyright © 2017 Wayne Ohmer. All rights reserved.
+//
+
+import XCTest
+@testable import Fyre_Dice
+
+class Fyre_DiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
